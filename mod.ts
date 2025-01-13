0b0703491cf637f47e7286db8fdb8f55f603aa0e
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-01-13 22:23:16 +0900

    fix: type not exported

--- mod.ts
+++ mod.ts
@@ -1,4 +1,14 @@
 import cmd from "./run.ts";
 import build from "./build.ts";
 
+export type {
+  BrowserInfo,
+  ChromiumOptions,
+  CMDOptions,
+  ExtTarget,
+  FirefoxOptions,
+  WebExtensionInfo,
+  WebExtensionInit,
+} from "./run.ts";
+
 export default { cmd, build };
